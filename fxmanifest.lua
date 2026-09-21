73c832fe321bc6c6b71bfacdeed371658c229ebc
RexShack <rexshackuk@gmail.com>
2026-09-21 11:23:23 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-track'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+    'server/versionchecker.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+dependencies {
+    'rsg-core',
+    'ox_lib',
+}
+
+files {
+  'locales/*.json'
+}
+
+lua54 'yes'
